26646f9cec3f139a06703207a0ce5f71f88717ae
MustiAlparslan <mustibatualp@gmail.com>
2022-08-18 21:05:42 +0300

    is Click ? True or False

--- src/Components/Text.js
+++
@@ -1,9 +0,0 @@
-export default class Text extends Component {
-  render() {
-    return (
-        <div>
-            <input type="text" onChange={(e) => setTodo(e.target.value)}/>
-        </div>
-    )
-  }
-}
