b62eb98feff474e94228e73b6b099487f0b03827
MustiAlparslan <mustibatualp@gmail.com>
2022-08-18 20:26:15 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import { useState } from "react";
-import Text from "./Components/Text";
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import {BrowserRouter as Router ,Link, Route, Routes } from "react-router-dom";
 
 function App() {
 
-  let [todo, setTodo] = useState([]);
+
+  const Home = () => {
+    return <div>Home</div>
+  }
+
+  const NotFound = () => {
+    return <div>NotFound!</div>
+  }
+
   return (
-    <div>
-      <Text todo={todo} setTodo={setTodo}/>    
-      <button type="button" onClick={() => console.log(todo)}>Ekle</button>
-    </div>
+    <Router>
+      <div>
+        <header className="w-full h-16 bg-sky-500 flex justify-around items-center	">
+          <Link to="/">
+            <h1 className="text-3xl text-white">LOGO</h1>
+          </Link>
+          <ul className="flex w-36 text-white justify-between">
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </header>
+
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact"t element={<Contact/>}/>
+            <Route path="*"t element={<NotFound/>}/>
+          </Routes>
+        </div>
+      
+      </div>
+    </Router>
   );
 }
 
