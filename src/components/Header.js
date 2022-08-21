6450e19a7c5b31beaa1cafc6876025ed2652f000
MustiAlparslan <mustibatualp@gmail.com>
2022-08-21 19:27:02 +0300

    Done !!

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom"
 function Header() {
   return (
     <div>
-        <header className="w-full h-16 bg-sky-500 flex justify-around items-center	">
+        <header className="w-full h-16 bg-sky-500  	">
           <Link to="/">
-            <h1 className="text-3xl text-white">LOGO</h1>
+            <h1 className="text-3xl text-white leading-[60px]	text-center ">TODO</h1>
           </Link>
           <ul className="flex w-36 text-white justify-between">
             <Link to="">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
 import Form from "../components/Form";
+import { TiTick } from "react-icons/ti";
+import { AiOutlineClose } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
 
 function Home() {
   const [todo, setTodo] = useState("");
   const [list, setList] = useState([]);
   let [count, setCount] = useState(1);
-  let getList = localStorage.getItem("todos");
-  let parseList = JSON.parse(getList);
+  
+    let getList = localStorage.getItem("TODOS");
+    let parseList = JSON.parse(getList);
 
-  const addLocalStorage = (e) => {
+    const addLocalStorage = (e) => {
     if (todo) {
       setCount(count + 1);
-      const newTask = { id: count, name: todo, status: false};
+      const newTask = { id: count, name: todo, status: false };
       setList([...list, newTask]);
-      localStorage.setItem("todos", JSON.stringify([...list, newTask]));
+      localStorage.setItem("TODOS", JSON.stringify([...list, newTask]));
     } else {
       console.log("YOK");
     }
     setTodo("");
   };
- 
 
   return (
     <div>
@@ -40,30 +43,32 @@ function Home() {
           {parseList.map((item, index) => (
             <li
               className="bg-white shadow-xl	py-2 px-4 opacity-95 rounded-[4px]	 shadow-[#818ab5]	  flex flex-col text-left  max-w-lg 	w-full  break-all my-8   px-[8px] min-h-[70px]  flex justify-between"
-              key={index}>
+              key={index}
+            >
               <h1 className="font-bold">TODO</h1>
-             <span className="italic">{item.name}</span>
+              <span className="italic">{item.name}</span>
 
               <div className="flex  justify-between mt-[15px] border-t text-[11px] text-gray-800 italic tracking-wide	text-italic">
-                <div className="flex mt-[4px]"
-                 onClick={() => {
-                  let status = list.map((itm) =>
-                    itm.id === item.id ? { ...itm, status: !itm.status } : itm
-                  );
-                  setList(status);
-                  localStorage.setItem("todos", JSON.stringify(status));
-                }}
+                <div
+                  className="flex mt-[4px]"
+                  onClick={() => {
+                    let status = list.map((itm) =>
+                      itm.id === item.id ? { ...itm, status: !itm.status } : itm
+                    );
+                    setList(status);
+                    localStorage.setItem("TODOS", JSON.stringify(status));
+                  }}
                 >
                   {item.status ? (
-                    <span className="bg-green-300 hover:cursor-pointer  rounded-[50px] w-[8px]  h-[8px] p-2.5"></span>
+                    <TiTick size={22} style={{ color: "green" }} />
                   ) : (
-                    <span className="bg-red-300  hover:cursor-pointer rounded-[50px] w-[8px] h-[8px] p-2.5"></span>
+                    <AiOutlineClose size={22} style={{ color: "red" }} />
                   )}
                 </div>
-                
-                <div 
+
+                <div
                   className="text-[14px] hover:cursor-pointer mt-[4px] px-[4px] ">
-                  Edit
+                  <MdDelete size={22} style={{color: "#0ea5e9"}}/>
                 </div>
               </div>
             </li>
